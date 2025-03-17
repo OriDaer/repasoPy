0aeb017b25f5c12d29035daae100fe6a9845868b
OriDaer <d47709740@alumnos.itr.edu.ar>
2025-03-17 16:30:29 -0300

    finnn

--- e8.py
+++ e8.py
@@ -8,7 +8,7 @@ while proceso==True:
         proceso=False
         break
     lista.append(names)
-lista.sort()
+
 if proceso==False:
     for i in lista:
         if i.startswith('e') or i.startswith('E'):
@@ -20,6 +20,6 @@ if proceso==False:
         print(f'el nombre {especifico} se encuentra en la lista')
     else:
         print(f'el nombre {especifico} no se encuentra en la lista')
-    print(lista)
+    print(sorted(lista))
     print('la cantidad de nombres que comienzan con e o E es: ',cant)
     print('la cantidad de nombres que comienzan con a o A es: ',cant2)
